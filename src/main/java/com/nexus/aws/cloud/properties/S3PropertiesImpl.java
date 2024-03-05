92ea22a219a48ccb5e7dc4aed09767cfab576745
igorcampos-dev <icampos.developer@yahoo.com>
2024-03-04 22:07:41 -0300

    refactor: melhorias e criacao da interface para serem chamadas, no lugar das classes concretas

--- src/main/java/com/nexus/aws/cloud/properties/S3PropertiesImpl.java
+++ src/main/java/com/nexus/aws/cloud/properties/S3PropertiesImpl.java
@@ -1,12 +1,14 @@
 package com.nexus.aws.cloud.properties;
 
-import lombok.Data;
+
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
+@Setter
 @Configuration
 @EnableConfigurationProperties
 @ConfigurationProperties(prefix = "spring.aws.credentials")
